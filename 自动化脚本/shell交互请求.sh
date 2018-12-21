dd8d6d59f83d6dae0e52a8f9fea89468dc3a866b
gouki777 <yf.sun@qq.com>
2018-12-21 17:29:36 +0800

    update

---
+++ 自动化脚本/shell交互请求.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+#清理24h不用的image
+(sleep 1
+echo "y"
+sleep 1)|docker image prune -a --filter "until=24h"
+#清理24h不用的docker网络
+(sleep 1
+echo "y"
+sleep 1)|docker network prune --filter "until=24h"
